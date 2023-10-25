4c40a3e3a0a797a6564b5d28c0d552e74b39c41a
Marc Muehlheim <marc.muehlheim@hftm.ch>
2023-10-25 10:37:11 +0200

    Add everything

--- src/component/ProgressBar.ts
+++ src/component/ProgressBar.ts
@@ -7,25 +7,30 @@ class ProgressBar extends HTMLElement {
   constructor() {
     super();
 
-    this.shadow = this.attachShadow({ mode: 'open' });
-    this.shadow.innerHTML = '<link rel="stylesheet" href="./assets/css/progressbar.css">';
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow.innerHTML =
+      '<link rel="stylesheet" href="./assets/css/progressbar.css">';
 
     // Create an element to add into the shadow DOM, the base
-    this.background = document.createElement('div');
-    this.background.setAttribute('class', 'sb-prog-bar');
+    this.background = document.createElement("div");
+    this.background.setAttribute("class", "sb-prog-bar");
 
-    this.progress = document.createElement('div');
-    this.progress.setAttribute('class', 'sb-progress');
-    this.progress.setAttribute('style', 'width: 0%');
+    this.progress = document.createElement("div");
+    this.progress.setAttribute("class", "sb-progress");
+    this.progress.setAttribute("style", "width: 0%");
 
-    this.progressValue = parseInt(this.getAttribute('progress') || '');
+    this.progressValue = parseInt(this.getAttribute("progress") || "50");
 
     this.background.appendChild(this.progress);
     this.shadow.appendChild(this.background);
   }
 
   connectedCallback(): void {
-    setTimeout(() => this.progress.setAttribute('style', `width: ${this.progressValue}%`), 1);
+    setTimeout(
+      () =>
+        this.progress.setAttribute("style", `width: ${this.progressValue}%`),
+      1
+    );
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,45 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.ts',
-  devtool: 'source-map',
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "source-map",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.svg?$/,
-        use: 'raw-loader',
+        use: "raw-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: 9000,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, "demo"),
+    },
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'assets', to: 'assets' }
-      ]
-    })
-  ]
+      patterns: [{ from: "assets", to: "assets" }],
+    }),
+  ],
 };
