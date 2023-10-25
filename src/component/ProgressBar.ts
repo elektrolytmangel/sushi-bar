3b18165e48b70b58101efebbfbe0add05ea5f7cf
Marc Muehlheim <marc.muehlheim@hftm.ch>
2023-10-25 13:57:53 +0200

    Update style

--- src/component/ProgressBar.ts
+++ src/component/ProgressBar.ts
@@ -9,8 +9,6 @@ class ProgressBar extends HTMLElement {
   constructor() {
     super();
     this.shadow = this.attachShadow({ mode: "open" });
-    this.shadow.innerHTML =
-      '<link rel="stylesheet" href="./assets/css/progressbar.css" mime-type="text/css">';
 
     // Create an element to add into the shadow DOM, the base
     this.background = document.createElement("div");
@@ -25,6 +23,7 @@ class ProgressBar extends HTMLElement {
   }
 
   connectedCallback(): void {
+    this.loadCss();
     this.updateStyle();
   }
 
@@ -48,6 +47,36 @@ class ProgressBar extends HTMLElement {
         `width: ${this.width}; height: ${this.height}`
       );
     }, 1);
+  }
+
+  loadCss() {
+    const style = document.createElement("style");
+    style.textContent = `
+      .sb-prog-bar {
+        background-color: transparent;
+        border: 1px solid #999999;
+        border-radius: 5rem;
+        
+        /*only fallback*/
+        height: 100%;
+        width: 100%;
+      }
+      
+      .sb-progress {
+        background-image: url('./assets/images/sushi-03-nigiri-maguro.svg'), linear-gradient(to right, rgba(255, 0, 0, 0.5), rgba(255, 165, 0, 0.5), rgba(255, 255, 0, 0.5), rgba(0, 128, 0, 0.5), rgba(0, 0, 255,0.5), rgba(238, 130, 238, 0.5));
+        /*, url('../images/sushi-02-nigiri-ebi.svg'), url('../images/sushi-03-nigiri-maguro.svg'), url('../images/sushi-03-nigiri-tamago.svg');*/
+        background-size: auto 100%;
+        background-position: right;
+        background-repeat: repeat-x, no-repeat;
+        height: 100%;
+        transition: width 1s;
+        border-radius: 5rem;
+        border: 0 solid transparent;
+      }
+    `;
+
+    // Attach the created elements to the shadow dom
+    this.shadow.appendChild(style);
   }
 }
 
